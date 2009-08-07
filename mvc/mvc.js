a50f09b3dcec5d64bde67959157f5832dcf4bd90
robert <robert@oatlab.com>
2009-08-07 11:06:09 -0400

    adding the m part

--- mvc/mvc.js
+++ mvc/mvc.js
@@ -35,6 +35,9 @@
 		return result;
 	}),
 	views = (mvc.views = get_views()),
+	get_view_string = (mvc.get_view_string = function (name) {
+		return views[name].innerHTML;
+	}),
 	get_view = (mvc.get_view = function (name) {
 
 		var matches = [], my_array, obj = {};
@@ -50,7 +53,89 @@
 		var view = views[name].cloneNode(true);
 		view.innerHTML = view.innerHTML[o.supplant](obj);
 		return view;
-	});
+	}),
+	resource = (mvc.resource = o.builder({
+		// implementer needs to provide these
+		// url: '/cards',
+		// name: 'card',
+		// token: 'asdf',
+		request: o.remote.request,
+		get_url_for_id: function (id) {
+			return this.url + '/{id}.json'[o.supplant]({id: id});
+		},
+		get_json_url: function () {
+			return this.url + '.json';
+		},
+		get_token_query_string: function () {
+			return 'token=' + this.token;
+		},
+		add_token_query_string: function (s) {
+			return s + (s.length ? '&' : '') + this.get_token_query_string();
+		},
+		get_token_param_obj: function () {
+			return {key: 'token', value: this.token};
+		},
+		all: function (callback) {
+			return this.request({
+				url: this.get_json_url(),
+				on_success: function (r) {
+					callback(o.json.parse(r.responseText)[o.map](o.get_object_property[o.rcurry](name)));
+				},
+				on_failure: function (r) {
+					callback(r);
+				}
+			});
+		},
+		get: function (id,callback) {
+			return this.request({
+				url: this.get_url_for_id(id),
+				on_success: function (r) {
+					callback(o.json.parse(r.responseText)[name])
+				},
+				on_failure: function (r) {
+					callback(r);
+				}
+			});
+		},
+		destroy: function (id,callback) {
+			return this.request({
+				url: this.get_url_for_id(id),
+				method: 'delete',
+				body: this.get_token_query_string(),
+				on_complete: callback
+			});
+		},
+		create: function (array_of_params,callback) {
+			return this.request({
+				url: this.get_json_url(),
+				method: 'post',
+				body: this.add_token_query_string(o.remote.query_string(array_of_params)),
+				on_complete: callback
+			});
+		},
+		update: function (id,array_of_params,callback) {
+			// js doesnt have multiple dispatch
+			var args = o.array(arguments), id, array_of_params, callback;
+			if (args.length === 3) {
+				id = args[0];
+				array_of_params = args[1];
+				callback = args[2];
+			} else {
+				id = args[0][o.find](function (param_obj) {
+					return param_obj.key === 'card[id]';
+				}).value;
+				array_of_params = args[0];
+				callback = args[1];
+			}
+
+			return this.request({
+				url: this.get_url_for_id(id),
+				method: 'put',
+				body: this.add_token_query_string(o.remote.query_string(array_of_params)),
+				on_complete: callback
+			});
+		}
+	}));
 
 	o.dom.event.delegate({
 		test: function (node) {
@@ -61,6 +146,5 @@
 			mvc.fire(target.getAttribute('action'),e,oe);
 		}
 	});
-
 	
 })();
