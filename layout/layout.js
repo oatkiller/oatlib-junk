76dcb2ed0908fb8de00cbafa96bab11ccb56c7b7
robert <robert@oatlab.com>
2009-08-09 15:34:55 -0400

    added layout module. helps auto height stuff in scruminator

---
+++ layout/layout.js
@@ -0,0 +1,32 @@
+//= require <oatlib-ui/reference>
+//= require <dom/element>
+//= require <dom/hide>
+//= require <dom/unhide>
+//= require <dom/event/add_listener>
+//= require <dom/debounce>
+//= require <dom/is_node>
+//= require <dom/capacitate>
+(function () {
+ 	var nodes = (o.ui.auto_layout_nodes = []),
+	handler = (o.ui.auto_layout_handler = function (my_node) {
+		my_nodes = o.dom.is_node(my_node) ? [my_node] : nodes;
+		my_nodes[o.each](o.dom.hide);
+		window.setTimeout(function () {
+			my_nodes[o.each](function (node) {
+				if (node.parentNode) {
+					node.style.height = node.parentNode.offsetHeight + 'px';
+					o.dom.unhide(node);
+				}
+			});
+		},0);
+	});
+	o.ui.auto_layout = (function (node) {
+		nodes.push(node);
+		o.ui.auto_layout_handler();
+		return function () {
+			nodes.splice(nodes[o.indexOf](node),1);
+			return true;
+		};
+	})[o.capacitate](0);
+	o.dom.event.add_listener(window,'resize',o.ui.auto_layout_handler[o.debounce]());
+})();
