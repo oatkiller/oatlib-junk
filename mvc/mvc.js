cce82cdd19d325e2c48665444e9a1526884bc8c2
robert <robert@oatlab.com>
2009-08-16 13:31:10 -0400

    split resource out of mvc

--- mvc/mvc.js
+++ mvc/mvc.js
@@ -1,14 +1,8 @@
 //= require <oatlib-ui/reference>
-//= require <dom/update>
 //= require <each>
 //= require <array>
-//= require <dom/element>
 //= require <dom/event/delegate>
 //= require <supplant>
-//= require <remote/query_string_from_obj>
-//= require <remote/request>
-//= require <json/parse>
-//= require <map>
 (function () {
 
 	var mvc = (o.ui.mvc = {});
@@ -59,100 +53,6 @@
 			tmp.innerHTML = tmp.innerHTML[o.supplant](get_empty_supplant_object(tmp.innerHTML));
 			return tmp.firstChild;
 		}),
-		resource = (mvc.resource = o.builder({
-			// implementer needs to provide these
-			// url: '/cards',
-			// name: 'card',
-			// token: 'asdf',
-	//	request: function () {
-	//		var that = this, args = o.array(arguments), request, timer = setTimeout(function () {
-	//			request = o.remote.request.apply(that,args);
-	//		},2000);
-	//		return {
-	//			abort: function () {
-	//				timer && clearTimeout(timer);
-	//				request && request.abort;
-	//			}
-	//		};
-	//	},
-			request: o.remote.request,
-			get_url_for_id: function (id) {
-				return this.url + '/{id}.json'[o.supplant]({id: id});
-			},
-			get_json_url: function () {
-				return this.url + '.json';
-			},
-			get_token_query_string: function () {
-				return 'authenticity_token=' + encodeURIComponent(this.token);
-			},
-			add_token_query_string: function (s) {
-				return s + (s.length ? '&' : '') + this.get_token_query_string();
-			},
-			get_token_param_obj: function () {
-				return {key: 'token', value: this.token};
-			},
-			get_object_from_response: function (r) {
-				return o.json.parse(r.responseText)[this.name];
-			},
-			all: function (callback) {
-				var that = this;
-				return this.request({
-					url: this.get_json_url(),
-					on_success: function (r) {
-						callback && callback(o.json.parse(r.responseText)[o.map](function (obj) {
-							return obj[that.name];
-						}));
-					},
-					on_failure: function (r) {
-						callback && callback(r);
-					}
-				});
-			},
-			get: function (id,callback) {
-				var that = this;
-				return this.request({
-					url: this.get_url_for_id(id),
-					on_success: function (r) {
-						callback && callback(that.get_object_from_response(r));
-					},
-					on_failure: function (r) {
-						callback && callback(r);
-					}
-				});
-			},
-			destroy: function (id,callback) {
-				return this.request({
-					url: this.get_url_for_id(id),
-					method: 'delete',
-					body: this.get_token_query_string(),
-					on_complete: callback
-				});
-			},
-			create: function (array_of_params,callbacks) {
-				var that = this;
-				return this.request({
-					url: this.get_json_url(),
-					method: 'post',
-					body: this.add_token_query_string(o.remote.query_string_from_obj(array_of_params)),
-					on_success: callbacks && callbacks.on_success && function (r) {
-						callbacks.on_success(o.json.parse(r.responseText)[that.name]);
-					},
-					on_complete: callbacks.on_complete,
-					on_failure: callbacks.on_failure
-				});
-			},
-			update: function (id,array_of_params,callback) {
-				var that = this;
-				return this.request({
-					url: this.get_url_for_id(id),
-					method: 'put',
-					body: this.add_token_query_string(o.remote.query_string_from_obj(array_of_params)),
-					on_success: function (r) {
-						callback && callback(that.get_object_from_response(r));
-					}
-				});
-			}
-		})),
 		default_delegate_test = (mvc.default_delegate_test = function (node) {
 			return node.getAttribute !== undefined && node.getAttribute('action') !== null;
 		}),
@@ -165,5 +65,4 @@
 			action: default_delegate_action
 		}));
 	};
-	
 })();
---
+++ resource/resource.js
@@ -0,0 +1,105 @@
+//= require <oatlib-ui/reference>
+//= require <builder>
+//= require <supplant>
+//= require <remote/request>
+//= require <remote/query_string_from_obj>
+//= require <json/parse>
+//= require <map>
+
+o.ui.resource = o.builder({
+	// implementer needs to provide these
+	
+	// url: '/cards',
+	// name: 'card',
+	// token: 'asdf',
+
+// use this instead of below to test lag
+//	request: function () {
+//		var that = this, args = o.array(arguments), request, timer = setTimeout(function () {
+//			request = o.remote.request.apply(that,args);
+//		},2000);
+//		return {
+//			abort: function () {
+//				timer && clearTimeout(timer);
+//				request && request.abort;
+//			}
+//		};
+//	},
+	request: o.remote.request,
+	get_url_for_id: function (id) {
+		return this.url + '/{id}.json'[o.supplant]({id: id});
+	},
+	get_json_url: function () {
+		return this.url + '.json';
+	},
+	get_token_query_string: function () {
+		return 'authenticity_token=' + encodeURIComponent(this.token);
+	},
+	add_token_query_string: function (s) {
+		return s + (s.length ? '&' : '') + this.get_token_query_string();
+	},
+	get_token_param_obj: function () {
+		return {key: 'token', value: this.token};
+	},
+	get_object_from_response: function (r) {
+		return o.json.parse(r.responseText)[this.name];
+	},
+	all: function (callback) {
+		var that = this;
+		return this.request({
+			url: this.get_json_url(),
+			on_success: function (r) {
+				callback && callback(o.json.parse(r.responseText)[o.map](function (obj) {
+					return obj[that.name];
+				}));
+			},
+			on_failure: function (r) {
+				callback && callback(r);
+			}
+		});
+	},
+	get: function (id,callback) {
+		var that = this;
+		return this.request({
+			url: this.get_url_for_id(id),
+			on_success: function (r) {
+				callback && callback(that.get_object_from_response(r));
+			},
+			on_failure: function (r) {
+				callback && callback(r);
+			}
+		});
+	},
+	destroy: function (id,callback) {
+		return this.request({
+			url: this.get_url_for_id(id),
+			method: 'delete',
+			body: this.get_token_query_string(),
+			on_complete: callback
+		});
+	},
+	create: function (array_of_params,callbacks) {
+		var that = this;
+		return this.request({
+			url: this.get_json_url(),
+			method: 'post',
+			body: this.add_token_query_string(o.remote.query_string_from_obj(array_of_params)),
+			on_success: callbacks && callbacks.on_success && function (r) {
+				callbacks.on_success(o.json.parse(r.responseText)[that.name]);
+			},
+			on_complete: callbacks.on_complete,
+			on_failure: callbacks.on_failure
+		});
+	},
+	update: function (id,array_of_params,callback) {
+		var that = this;
+		return this.request({
+			url: this.get_url_for_id(id),
+			method: 'put',
+			body: this.add_token_query_string(o.remote.query_string_from_obj(array_of_params)),
+			on_success: function (r) {
+				callback && callback(that.get_object_from_response(r));
+			}
+		});
+	}
+});
