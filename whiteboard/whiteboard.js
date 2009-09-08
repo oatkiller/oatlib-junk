9aab02bda7f82fab68df1c7749be494ae92b92c3
robert <robert@oatlab.com>
2009-09-08 00:52:05 -0400

    working but needs performance tweaks

--- whiteboard/whiteboard.js
+++ whiteboard/whiteboard.js
@@ -5,6 +5,8 @@
 //= require <dom/remove>
 //= require <dom/find_position>
 //= require <map>
+//= require <last>
+//= require <error>
 
 o.ui.whiteboard = function () {
 
@@ -20,31 +22,78 @@ o.ui.whiteboard = function () {
 		path_context: get_context(),
 		tool_context: get_context(),
 		presentation_context: get_context(),
+		paths: [],
 		update: function () {
 			if (this.previous_paths_length !== undefined) {
-				if (this.previous_paths_length > this.paths.length) {
-					this.paths.draw();
-				} else if (this.previous_paths_length < this.paths.length) {
-					clear_ctx(this.path_context);
-					this.paths[o.each](function (path) {
-						path.draw();
-					});
+				if (this.previous_paths_length === this.paths.length - 1) {
+					console.log('drawing the latest path');
+					this.paths[o.last]().draw();
 				}
 			}
+			this.previous_paths_length = this.paths.length;
 			clear_ctx(this.presentation_context);
 			this.presentation_context.drawImage(this.path_context.canvas,0,0);
 			this.presentation_context.drawImage(this.tool_context.canvas,0,0);
 		}
 	},
+	path_builder = o.builder({
+		add_point: function (point) {
+			(this.points || (this.points = [])).push({
+				x: point.x,
+				y: point.y
+			});
+		},
+		draw: function (ctx) {
+			if (this.points === undefined || this.points.length < 2) {return false;}
+			ctx = ctx || board.path_context;
+			ctx.save();
+
+			ctx.lineWidth = this.settings.line_width;
+			ctx.lineCap = this.settings.line_cap;
+			ctx.lineJoin = this.settings.line_join;
+			ctx.strokeStyle = this.settings.stroke_style;
+
+			ctx.beginPath();
+			var points = this.points.slice(0),
+			point = points.shift();
+			ctx.moveTo(point.x,point.y);
+			while (points.length) {
+				point = points.shift();
+				ctx.lineTo(point.x,point.y);
+			}
+			ctx.stroke();
+			ctx.restore();
+		},
+		save: function () {
+			board.paths.push(this);
+			delete tool.edit_path;
+		}
+	}),
 	tool = {
+		get_path: function (settings) {
+			if (this.edit_path) {
+				return this.edit_path;
+			} else {
+				this.edit_path = path_builder({
+					settings: o.combine({},this.settings),
+					points: []
+				});
+				this.edit_path.add_point(this);
+			}
+		},
 		settings: {
-			line_width: 10,
-			stroke_style: '#000000'
+			line_width: 30,
+			stroke_style: '#000000',
+			line_cap: 'round',
+			line_join: 'round'
 		},
 		set_point_from_mouse_coordinates: function (coordinates) {
 			var position = o.dom.find_position(board.presentation_context.canvas);
 			this.x = coordinates.x - position.x;
 			this.y = coordinates.y - position.y;
+			if (this.edit_path) {
+				this.edit_path.add_point(this);
+			}
 		},
 		draw: function () {
 			clear_ctx(board.tool_context);
@@ -56,11 +105,12 @@ o.ui.whiteboard = function () {
 			board.update();
 		},
 		draw_shadow: function () {
+			console.log('drawing the shadow');
 			var ctx = board.tool_context;
 			ctx.save();
 			ctx.strokeStyle = 'rgba(0,0,0,.5)';
 			ctx.beginPath();
-			ctx.arc(this.x,this.y,this.settings.line_width,0,Math.PI*2,true);
+			ctx.arc(this.x,this.y,this.settings.line_width / 2,0,Math.PI*2,true);
 			ctx.stroke();
 			ctx.restore();
 		}
@@ -70,5 +120,20 @@ o.ui.whiteboard = function () {
 		this.set_point_from_mouse_coordinates(oe.get_mouse_coordinates());
 		this.draw();
 	}[o.bind](tool));
+	o.dom.event.add_listener(board.presentation_context.canvas,'mousedown',function (e,oe) {
+		this.set_point_from_mouse_coordinates(oe.get_mouse_coordinates());
+		this.get_path();
+		this.draw();
+	}[o.bind](tool));
+	o.dom.event.add_listener(document,'mouseup',function (e,oe) {
+		this.edit_path && this.edit_path.save();
+		this.draw();
+	}[o.bind](tool));
+
+/*
+	['tool','path'][o.each](function (a) {
+		o.dom.remove(board[a+'_context'].canvas);
+	});
+	*/
 
 };
