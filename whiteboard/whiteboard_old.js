b5b048f94899f88dad4609a9ab4585c9e64f5967
robert <robert@oatlab.com>
2009-09-08 00:26:25 -0400

    got the shadow working

--- whiteboard/whiteboard_old.js
+++ whiteboard/whiteboard_old.js
@@ -6,17 +6,16 @@
 //= require <dom/find_position>
 //= require <map>
 
-o.ui.whiteboard = function () {
+o.ui.whiteboard = function (width,height) {
 
-	var width = 600,
-	height = 600,
-	get_context = o.ui.canvas.get_context[o.curry](width,height),
+	var get_context = o.ui.canvas.get_context[o.curry](width,height),
 	clear_ctx = function (ctx) {
 		var canvas = ctx.canvas;
 		ctx.clearRect(0,0,canvas.width,canvas.height);
 		return ctx;
 	},
 	board = {
+		paths: [],
 		path_context: get_context(),
 		tool_context: get_context(),
 		presentation_context: get_context(),
@@ -31,12 +30,51 @@ o.ui.whiteboard = function () {
 					});
 				}
 			}
-			clear_ctx(this.presentation_context);
-			this.presentation_context.drawImage(this.path_context.canvas,0,0);
-			this.presentation_context.drawImage(this.tool_context.canvas,0,0);
+			clear_ctx(presentation_context);
+			presentation_context.drawImage(this.path_context.canvas,0,0);
+			presentation_context.drawImage(this.tool_context.canvas,0,0);
 		}
 	},
+	path_builder = o.builder({
+		add_point: function (point) {
+			(this.points || (this.points = [])).push(point);
+		},
+		draw: function (ctx) {
+			if (this.points === undefined) {return false;}
+			ctx = ctx || board.path_context;
+			ctx.save();
+
+			ctx.lineWidth = this.settings.line_width;
+			ctx.strokeStyle = this.settings.stroke_style;
+
+			ctx.beginPath();
+			var points = this.points.slice(0),
+			point = points.shift();
+			ctx.moveTo(point.x,point.y);
+			while (points.length) {
+				point = points.shift();
+				ctx.lineTo(point.x,point.y);
+			}
+			ctx.stroke();
+			ctx.restore();
+		},
+		save: function () {
+			board.paths.push(this);
+			delete tool.edit_path;
+		}
+	}),
 	tool = {
+		get_path: function (settings) {
+			return this.edit_path || (this.edit_path = path_builder({
+				settings: o.combine({},this.settings),
+				points: [
+					{
+						x: this.x,
+						y: this.y
+					}
+				]
+			}));
+		},
 		settings: {
 			line_width: 10,
 			stroke_style: '#000000'
@@ -58,16 +96,27 @@ o.ui.whiteboard = function () {
 		draw_shadow: function () {
 			var ctx = board.tool_context;
 			ctx.save();
-			ctx.strokeStyle = 'rgba(0,0,0,.5)';
+			ctx.fillStyle = 'rgba(0,0,0,.5)';
 			ctx.beginPath();
 			ctx.arc(this.x,this.y,this.settings.line_width,0,Math.PI*2,true);
 			ctx.stroke();
 			ctx.restore();
+		},
+		end_path: function () {
 		}
 	};
 
 	o.dom.event.add_listener(board.presentation_context.canvas,'mousemove',function (e,oe) {
 		this.set_point_from_mouse_coordinates(oe.get_mouse_coordinates());
+		this.draw();
+	}[o.bind](tool));
+	o.dom.event.add_listener(board.presentation_context.canvas,'mousedown',function (e,oe) {
+		this.set_point_from_mouse_coordinates(oe.get_mouse_coordinates());
+		this.get_path();
+		this.draw();
+	})[o.bind](tool);
+	o.dom.event.add_listener(document,'mouseup',function (e,oe) {
+		this.edit_path && this.edit_path.save();
 		this.draw();
 	}[o.bind](tool));
 
